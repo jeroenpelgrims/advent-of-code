6ad0022d08fd640192cef9f6c3368c933923d4d2
Jeroen Pelgrims <jeroen.pelgrims@gmail.com>
2021-12-07 01:03:37 +0100

    Rewrote code to calculate both first and last winner

--- 2021-12-04 - Giant Squid/index.ts
+++ 2021-12-04 - Giant Squid/index.ts
@@ -1,9 +1,10 @@
 import { readFileSync } from "fs";
 import { EOL } from "os";
-import { flatten, sum, transpose } from "ramda";
+import { flatten, head, last, sum, tail, transpose } from "ramda";
 
 type Grid = string[][];
-type State = { grids: Grid[], winner?: Grid, lastNumber?: string };
+type WinningGrid = { grid: Grid, lastNumber: string }
+type State = { grids: Grid[], winners: WinningGrid[] };
 
 function input(): [string[], Grid[]] {
     const data = readFileSync("./input")
@@ -35,19 +36,30 @@ function isWinner(grid: Grid) {
     return rowWinner || colWinner;
 }
 
+function sumGrid(grid: Grid) {
+    const numbers = flatten(grid)
+        .filter(x => x !== '')
+        .map(x => parseInt(x));
+    return sum(numbers);
+}
+
 const [numbers, grids] = input();
-const { winner, lastNumber } = numbers.reduce((state: State, number: string) => {
-    if (state.winner !== undefined) {
+const { winners } = numbers.reduce((state: State, number: string): State => {
+    if (state.grids.length === 0) {
         return state;
     }
 
     const grids = state.grids.map(grid => removeNumber(grid, number));
-    const winner = grids.find(isWinner);
-    return { ...state, grids, winner, lastNumber: number };
-}, { grids } as State);
+    const newWinners = grids.filter(isWinner);
 
-const restNumbers = flatten(winner!)
-    .filter(x => x !== '')
-    .map(x => parseInt(x));
+    return {
+        grids: grids.filter(grid => !newWinners.includes(grid)),
+        winners: [...state.winners, ...newWinners.map(grid => ({ grid, lastNumber: number }))]
+    };
+}, { grids, winners: [] });
 
-console.log("part1", sum(restNumbers) * parseInt(lastNumber!));
+const firstWinner = head(winners)!;
+console.log("part1", sumGrid(firstWinner.grid) * parseInt(firstWinner.lastNumber));
+
+const lastWinner = last(winners)!;
+console.log("part2", sumGrid(lastWinner.grid) * parseInt(lastWinner.lastNumber));
