c57bae3342ef0fe12e76fd8e6085cea6a31e91d0
Jeroen Pelgrims <jeroen.pelgrims@gmail.com>
2022-10-30 12:01:32 +0100

    Add 2015 2 part 1

---
+++ 2015/2/src/main.rs
@@ -0,0 +1,79 @@
+use std::cmp;
+use std::fs;
+
+type Dimensions = (i32, i32, i32);
+type Areas = (i32, i32, i32);
+
+fn parse_dimensions(text: &str) -> Dimensions {
+    let digits: Vec<i32> = text.split('x').map(|s| s.parse().unwrap()).collect();
+    (digits[0], digits[1], digits[2])
+}
+
+fn areas((w, b, h): Dimensions) -> Areas {
+    (w * b, b * h, w * h)
+}
+
+fn min_area((a1, a2, a3): Areas) -> i32 {
+    cmp::min(cmp::min(a1, a2), a3)
+}
+
+fn area(text: &str) -> i32 {
+    let (a1, a2, a3) = areas(parse_dimensions(text));
+    let min = min_area((a1, a2, a3));
+    (a1 + a2 + a3) * 2 + min
+}
+
+fn main() {
+    let contents = fs::read_to_string("./input.txt").unwrap();
+    let total = contents.lines().map(area).reduce(|a, b| a + b).unwrap();
+    print!("{}", total);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_size_2x3x4() {
+        let result = parse_dimensions("2x3x4");
+        assert_eq!(result.0, 2);
+        assert_eq!(result.1, 3);
+        assert_eq!(result.2, 4);
+    }
+
+    #[test]
+    fn parse_size_1x1x10() {
+        let result = parse_dimensions("1x1x10");
+        assert_eq!(result.0, 1);
+        assert_eq!(result.1, 1);
+        assert_eq!(result.2, 10);
+    }
+
+    #[test]
+    fn areas_2x3x4() {
+        let (a1, a2, a3) = areas(parse_dimensions("2x3x4"));
+        assert_eq!(a1, 2 * 3);
+        assert_eq!(a2, 3 * 4);
+        assert_eq!(a3, 2 * 4);
+    }
+
+    #[test]
+    fn areas_1x1x10() {
+        let (a1, a2, a3) = areas(parse_dimensions("1x1x10"));
+        assert_eq!(a1, 1 * 1);
+        assert_eq!(a2, 1 * 10);
+        assert_eq!(a3, 1 * 10);
+    }
+
+    #[test]
+    fn test_min_area() {
+        assert_eq!(min_area(areas(parse_dimensions("2x3x4"))), 6);
+        assert_eq!(min_area(areas(parse_dimensions("1x1x10"))), 1);
+    }
+
+    #[test]
+    fn test_area() {
+        assert_eq!(area("2x3x4"), 58);
+        assert_eq!(area("1x1x10"), 43);
+    }
+}
