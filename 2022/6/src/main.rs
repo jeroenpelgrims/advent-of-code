34a5839fba25c26f02c89d497a431510292e59a1
Jeroen Pelgrims <jeroen.pelgrims@gmail.com>
2022-12-30 21:00:46 +0100

    Add 2022 day 6 part 2

--- 2022/6/src/main.rs
+++ 2022/6/src/main.rs
@@ -6,10 +6,10 @@ fn all_different(input: &str) -> bool {
     input.chars().unique().count() == input.len()
 }
 
-fn marker(data: &str) -> i32 {
-    let result = (3..data.len()).fold(None, |result, i| match result {
+fn marker(data: &str, num_unique: usize) -> i32 {
+    let result = ((num_unique - 1)..data.len()).fold(None, |result, i| match result {
         None => {
-            let slice = &data[i - 3..i + 1];
+            let slice = &data[i - (num_unique - 1)..i + 1];
             if all_different(slice) {
                 Some(i)
             } else {
@@ -23,7 +23,8 @@ fn marker(data: &str) -> i32 {
 
 fn main() {
     let input = fs::read_to_string("./input.txt").unwrap();
-    print!("part 1: {}", marker(input.as_str()));
+    println!("part 1: {}", marker(input.as_str(), 4));
+    println!("part 2: {}", marker(input.as_str(), 14));
 }
 
 #[cfg(test)]
@@ -34,7 +35,7 @@ mod tests {
     #[test]
     fn test_marker() {
         let input = fs::read_to_string("./test-input.txt").unwrap();
-        let pos = marker(input.as_str());
+        let pos = marker(input.as_str(), 4);
         assert_eq!(pos, 7);
     }
 
