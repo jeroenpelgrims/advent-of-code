c2752d4e04df2a86de032e1505ccfb53a884b558
Jeroen Pelgrims <git@jeroenpelgrims.com>
2024-12-03 20:38:59 +0100

    Add 2024 day 3 part 2

--- 2024/3/src/main.rs
+++ 2024/3/src/main.rs
@@ -1,45 +1,67 @@
-use std::fs;
+use core::panic;
+use std::{fs, iter::once};
 
 use regex::Regex;
 
-#[derive(Debug)]
-struct Operation {
-    func: String,
-    x: i32,
-    y: i32,
+#[derive(Debug, Clone)]
+enum Operation {
+    Mul(i32, i32),
+    Do,
+    Dont,
 }
 
 fn find_operations(input: &str) -> Vec<Operation> {
-    let re = Regex::new(r"(mul)\((\d+),(\d+)\)").unwrap();
+    let re = Regex::new(r"(mul|do|don't)\(((\d+),(\d+))?\)").unwrap();
     let matches: Vec<Operation> = re
         .captures_iter(input)
         .map(|m| {
-            let func = m[1].to_string();
-            let x: i32 = m[2].parse().unwrap();
-            let y: i32 = m[3].parse().unwrap();
-            Operation {
-                func: func,
-                x: x,
-                y: y,
+            let func = &m[1];
+            match func {
+                "mul" => {
+                    let x: i32 = m[3].parse().unwrap();
+                    let y: i32 = m[4].parse().unwrap();
+                    Operation::Mul(x, y)
+                }
+                "do" => Operation::Do,
+                "don't" => Operation::Dont,
+                _ => panic!("Unsupported operation found: {:?}", &m[0]),
             }
         })
         .collect();
     matches
 }
 
-fn apply(operations: Vec<Operation>) -> i32 {
-    operations.iter().fold(0, |total, operation| {
-        match operation.func.as_str() {
-            "mul" => total + operation.x * operation.y,
-            _ => panic!("Unsupported operation: {:?}", operation),
+fn drop_donts(xs: &Vec<Operation>, dropping: bool) -> Vec<Operation> {
+    match &xs[..] {
+        [Operation::Dont, rest @ ..] => drop_donts(&rest.to_vec(), true),
+        [Operation::Do, rest @ ..] => drop_donts(&rest.to_vec(), false),
+        [op @ Operation::Mul(_, _), rest @ ..] => {
+            if dropping {
+                drop_donts(&rest.to_vec(), dropping)
+            } else {
+                let rest_dropped = drop_donts(&rest.to_vec(), dropping);
+                once(op.clone()).chain(rest_dropped).collect()
+            }
         }
-    })
+        [] => vec![],
+    }
+}
+
+fn apply(operations: &Vec<Operation>) -> i32 {
+    operations
+        .iter()
+        .fold(0, |total, operation| match operation {
+            Operation::Mul(x, y) => total + x * y,
+            _ => total,
+        })
 }
 
 fn main() {
     let input = fs::read_to_string("./input.txt").unwrap();
     let operations = find_operations(&input);
-    println!("1: {:?}", apply(operations));
+
+    println!("1: {:?}", apply(&operations));
+    println!("2: {:?}", apply(&drop_donts(&operations, false)));
 }
 
 #[cfg(test)]
